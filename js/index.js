cd855064b7255573a3ee0ae4bd03b1cd79d56f96
winstonplavizlatni <winstonplavizlatni@gmail.com>
2022-03-16 18:11:25 +0100

    16/03/22

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$('#platform-btn').click(function () {
+    $('#platform-container').stop(true).fadeToggle()
+    $('#platform-bottom-border').stop(true).fadeToggle();
+    $('#navbar-bg').stop(true).fadeToggle();
+});
