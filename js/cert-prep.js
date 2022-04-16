d228cb1f6d62b2d19579e664de011d5f2e4e1ed3
winstonplavizlatni <winstonplavizlatni@gmail.com>
2022-04-16 19:27:11 +0200

    16/04

--- js/cert-prep.js
+++ js/cert-prep.js
@@ -36,22 +36,3 @@ function trialWindowSubsPlus() {
     document.getElementById('team-trial-subs').innerHTML = teamTrialSubsNum;
   }
 }
-
-/* Hands-on demo video */
-function showCsVideo() {
-    document.getElementById('customer-stories-video').style.display = 'block';
-    document.getElementById('customer-stories-video-bg').style.display = 'block';
-    scrollTop = 
-              window.pageYOffset || document.documentElement.scrollTop;
-            scrollLeft = 
-              window.pageXOffset || document.documentElement.scrollLeft,
-              window.onscroll = function() {
-                window.scrollTo(scrollLeft, scrollTop);
-              }
-}
-
-function hideCsVideo() {
-    document.getElementById('customer-stories-video').style.display = 'none';
-    document.getElementById('customer-stories-video-bg').style.display = 'none';
-    window.onscroll = function() {};
-}
